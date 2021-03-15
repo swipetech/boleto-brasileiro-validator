31c829b04a2d5a22842fc68ead4e4cb299f17ced
Joseildo Filho <filhojoseildo@gmail.com>
2021-03-15 15:44:48 -0300

    changing files extensions

--- src/boleto.ts
+++ src/boleto.ts
@@ -2,7 +2,7 @@ import { boletoArrecadacao } from './boleto-arrecadacao';
 import { boletoBancario } from './boleto-bancario';
 import { clearMask } from './utils';
 
-export function boleto(codigo, validarBlocos = false) {
+export function boleto(codigo: string, validarBlocos:boolean = false): boolean {
   const cod = clearMask(codigo);
   if (Number(cod[0]) === 8) return boletoArrecadacao(cod, validarBlocos);
   return boletoBancario(cod, validarBlocos);
